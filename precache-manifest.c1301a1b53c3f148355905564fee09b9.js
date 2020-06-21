f4360adc1e7dc401b4fbf2f432836d3ebe3cdabc
WincherZ <wincher2016@gmail.com>
2020-06-21 14:50:35 +0300

    Updates

---
+++ precache-manifest.c1301a1b53c3f148355905564fee09b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1ca7b9aa2af9f2c171f55da5d75f16",
+    "url": "/instasport/index.html"
+  },
+  {
+    "revision": "465c0a4899a5df97b9e9",
+    "url": "/instasport/static/css/main.b239e866.chunk.css"
+  },
+  {
+    "revision": "1f5ea4cc18ef8fd80dd1",
+    "url": "/instasport/static/js/2.06f05f14.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/instasport/static/js/2.06f05f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465c0a4899a5df97b9e9",
+    "url": "/instasport/static/js/main.0af19f66.chunk.js"
+  },
+  {
+    "revision": "72a76cecac2d2338e4a0",
+    "url": "/instasport/static/js/runtime-main.a29f23fe.js"
+  }
+]);
